58b072265ae4d810d355564341bb2fe70fdc93c1
Alexander Bobovnikov <jeinor22@gmail.com>
2024-04-24 00:58:56 +0300

    Adding comments

--- dfs/Program.cs
+++ dfs/Program.cs
@@ -11,11 +11,13 @@ namespace dfs
 
         LinkedList<int>[] linkedListArray;
 
+        //Creating a graph
         public Graph(int v)
         {
             linkedListArray = new LinkedList<int>[v];
         }
 
+        //Filling out the graph
         public void AddEdge(int u, int v, bool blnBiDir = true)
         {
             if (linkedListArray[u] == null)
@@ -44,6 +46,7 @@ namespace dfs
             }
         }
 
+        //Graph Traversal
         internal void DFSHelper(int start, int finish, bool[] visited, ref int count)
         {
             visited[start] = true;
@@ -63,6 +66,7 @@ namespace dfs
             }
         }
 
+        //Depth First Search
         internal void DFS()
         {
             Console.WriteLine("DFS");
